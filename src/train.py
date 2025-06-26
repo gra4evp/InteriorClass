91cfd731f5324aac1a5b705875d71f30a110f839
gra4evp <petrgrachev1@gmail.com>
2025-06-27 01:44:31 +0300

    trainer v2 (not working)

--- src/train.py
+++ src/train.py
@@ -1,27 +1,123 @@
 from pathlib import Path
-import sys
 import json
-from src.config import TrainingConfig
+
+from tqdm import tqdm
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from sklearn.metrics import classification_report
+import torchvision.transforms as transforms
+from PIL import Image
+
+from src.config import RANDOM_SEED, SPLIT_RATIO, MIN_VAL_TEST_PER_CLASS, CLASS_LABELS
+from src.dataset.splitter import DatasetSplitter
+from src.dataset.interior_dataset import InteriorDataset, get_transforms
+from src.models.interior_classifier_EfficientNet_B3 import InteriorClassifier
 from src.trainer import Trainer
 
+
 if __name__ == "__main__":
-    # Пример: инициализация конфига (можно заменить на чтение из файла)
-    config = TrainingConfig(
-        data_dir=str(Path.cwd().parent.parent / "data" / "interior_dataset"),
-        exp_dir=str(Path.cwd().parent.parent / "experiments" / "exp001_baseline" / "results"),
-        batch_size=32,
-        epochs=10,
-        lr=3e-5,
-        img_size=380,
-        # Можно добавить другие параметры при необходимости
+
+    # 1. Собираем все пути
+    current_dir = Path.cwd()
+    root_project = current_dir.parent
+    data_dir = root_project / "data"
+    print(f"data_dir: {data_dir}")
+
+    dataset_dir = data_dir / "interior_dataset"
+
+    samples = InteriorDataset.collect_samples(dataset_dir=dataset_dir)
+    print(f"Total samples: {len(samples)}")
+
+    # 2. Создание сплиттера
+    splitter = DatasetSplitter(
+        class_labels=CLASS_LABELS,
+        split_config=SPLIT_RATIO,
+        random_seed=RANDOM_SEED
     )
 
-    # Сохраняем конфиг эксперимента в JSON
-    config_path = Path(config.exp_dir) / "training_config.json"
-    with open(config_path, "w") as f:
-        f.write(config.json(indent=4, ensure_ascii=False))
-    print(f"Training config saved to {config_path}")
+    # Разделение данных
+    train_samples, val_samples, test_samples = splitter.split(samples, shuffle=True)
+    print(f"Train samples: {len(train_samples)}")
+    print(f"Val samples: {len(val_samples)}")
+    print(f"Test samples: {len(test_samples)}")
+
+    # Конфигурация
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    BATCH_SIZE = 32
+    EPOCHS = 10
+    LR = 3e-5
+    IMG_SIZE = 380  # Для EfficientNet-B3
+    EXP_NUMBER = 2
+
+    # 3. Datasets
+    train_dataset = InteriorDataset(
+        train_samples,
+        transform=get_transforms(mode='train'),
+        mode='train'
+    )
+
+    val_dataset = InteriorDataset(
+        val_samples,
+        transform=get_transforms(mode='val'),
+        mode='val'
+    )
+
+    test_dataset = InteriorDataset(
+        test_samples,
+        transform=get_transforms(mode='test'),
+        mode='test'
+    )
+
+
+    # 4. DataLoaders
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True,
+        drop_last=True
+    )
+
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True
+    )
+
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True
+    )
+
+    model = InteriorClassifier(num_classes=len(CLASS_LABELS)).to(DEVICE)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), lr=LR)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=EPOCHS)
+
+    experiments_dir = root_project / "experiments"
+    exp_dir = experiments_dir / f"exp{EXP_NUMBER:03d}"
+    exp_results_dir = exp_dir / "results"
+    exp_results_dir.mkdir(parents=True, exist_ok=True)
 
     # Запуск обучения
-    trainer = Trainer(config)
-    trainer.train()
+    trainer = Trainer(
+        model=model,
+        criterion=criterion,
+        optimizer=optimizer,
+        sheduler=scheduler,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        test_loader=test_loader,
+        epochs=EPOCHS,
+        device=DEVICE,
+        exp_results_dir=exp_results_dir
+
+    )
+    model = trainer.train()
--- src/trainer.py
+++ src/trainer.py
@@ -1,62 +1,69 @@
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Tuple
 import json
 from tqdm import tqdm
 import torch
-from torch import nn, optim
+
 from torch.utils.data import DataLoader
 from sklearn.metrics import classification_report
 from src.config import TrainingConfig
-from src.dataset.interior_dataset import InteriorDataset, get_transforms
-from src.models.interior_classifier_EfficientNet_B3 import InteriorClassifier
-from src.dataset.splitter import DatasetSplitter
 from pydantic import BaseModel
 
-from torch.optim.optimizer import Optimizer
 
-class HyperparametersConfig(BaseModel):
+class OptimizerConfig(BaseModel):
+    name: str  # "Adam", "SGD", etc.
+    params: Dict[str, Any]  # {"lr": 0.001, "weight_decay": 0.01}
+
+
+class SchedulerConfig(BaseModel):
+    name: str  # "StepLR", "CosineAnnealingLR", etc.
+    params: Dict[str, Any]  # {"step_size": 30, "gamma": 0.1}
+
+
+class HyperParametersConfig(BaseModel):
     batch_size: int
     epochs: int
-    lr: float
     img_size: int
     random_seed: int
     device: str
-    criterion: torch.nn.CrossEntropyLoss
-    optimizer: torch.optim.Optimizer
-    scheduler: torch.optim.lr_scheduler.LRScheduler
+    criterion: str  # "CrossEntropyLoss", "MSELoss", etc.
+    optimizer: OptimizerConfig
+    scheduler: SchedulerConfig | None = None
 
 
 
 class Trainer:
     def __init__(
             self,
-            batch_size: int,
+            model: torch.nn.Module,
+            criterion: torch.nn.CrossEntropyLoss,
+            optimizer: torch.optim.Optimizer,
+            sheduler: torch.optim.lr_scheduler.LRScheduler,
+            train_loader: torch.utils.data.DataLoader,
+            val_loader: torch.utils.data.DataLoader,
+            test_loader: torch.utils.data.DataLoader,
             epochs: int,
-            lr: float,
-            img_size: int,
-            random_seed: int,
             device: str,
-            dataset_dir: Path,
-            exp_dir: Path
+            exp_results_dir: Path
         ):
-        self.batch_size = batch_size
+
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = sheduler
+
         self.epochs = epochs
-        self.lr = lr
-        self.img_size = img_size
-        self.random_seed = random_seed
         self.device = device
-        self.dataset_dir = dataset_dir
-        self.exp_dir = exp_dir
+
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.test_loader = test_loader
 
         # Save paths
-        self.results_dir = exp_dir / "results"
-        self.checkpoint_path = self.results_dir / "best_model.pth"
-        self.log_path = self.results_dir / "training_log.json"
+        self.exp_results_dir = exp_results_dir
+        self.checkpoint_path = exp_results_dir / "best_model.pth"
+        self.log_path = exp_results_dir/ "training_log.json"
 
-        self._init_paths()
-        self._init_datasets()
-        self._init_loaders()
-        self._init_model()
         self.log_dict: Dict[str, Any] = {
             "train_loss": [],
             "val_loss": [],
@@ -65,89 +72,6 @@ class Trainer:
         }
         self.best_val_loss = float("inf")
         self._load_log()
-
-    def _init_paths(self) -> None:
-        self.data_dir = Path(self.data_dir)
-        self.exp_dir = Path(self.exp_dir)
-        self.exp_dir.mkdir(parents=True, exist_ok=True)
-        self.checkpoint_path = Path(self.checkpoint_path) if self.checkpoint_path else self.exp_dir / "best_model.pth"
-        self.log_path = Path(self.log_path) if self.log_path else self.exp_dir / "training_log.json"
-
-    def _init_datasets(self) -> None:
-        samples = InteriorDataset.collect_samples(dataset_dir=self.data_dir)
-        splitter = DatasetSplitter(
-            class_labels=self.class_labels,
-            split_config=self.split_ratio,
-            random_seed=self.random_seed
-        )
-        train_samples, val_samples, test_samples = splitter.split(samples, shuffle=True)
-        self.train_dataset = InteriorDataset(
-            train_samples,
-            transform=get_transforms(mode='train'),
-            mode='train'
-        )
-        self.val_dataset = InteriorDataset(
-            val_samples,
-            transform=get_transforms(mode='val'),
-            mode='val'
-        )
-        self.test_dataset = InteriorDataset(
-            test_samples,
-            transform=get_transforms(mode='test'),
-            mode='test'
-        )
-
-    def _init_loaders(self) -> None:
-        self.train_loader = DataLoader(
-            dataset=self.train_dataset,
-            batch_size=self.config.batch_size,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=True,
-            drop_last=True
-        )
-        self.val_loader = DataLoader(
-            dataset=self.val_dataset,
-            batch_size=self.config.batch_size,
-            shuffle=False,
-            num_workers=4,
-            pin_memory=True
-        )
-        self.test_loader = DataLoader(
-            dataset=self.test_dataset,
-            batch_size=self.config.batch_size,
-            shuffle=False,
-            num_workers=4,
-            pin_memory=True
-        )
-
-    def _init_model(self) -> None:
-        self.model = InteriorClassifier(num_classes=len(self.class_labels)).to(self.device)
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = optim.AdamW(self.model.parameters(), lr=self.lr)
-        self.scheduler = optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=self.epochs)
-
-    def _load_log(self) -> None:
-        if self.log_path.exists():
-            with open(self.log_path, "r") as f:
-                self.log = json.load(f)
-            self.best_val_loss = self.log.get("best_val_loss", float("inf"))
-
-    def save_log(self) -> None:
-        with open(self.log_path, "w") as f:
-            json.dump(self.log, f, indent=4)
-
-    def save_checkpoint(self, epoch: int, val_loss: float, val_accuracy: float) -> None:
-        torch.save(
-            {
-                'epoch': epoch,
-                'model_state_dict': self.model.state_dict(),
-                'optimizer_state_dict': self.optimizer.state_dict(),
-                'loss': val_loss,
-                'accuracy': val_accuracy
-            }
-            , self.checkpoint_path
-        )
 
     def train_epoch(self, epoch: int) -> float:
         self.model.train()
@@ -165,21 +89,25 @@ class Trainer:
             loss = self.criterion(outputs, labels)
             loss.backward()
             self.optimizer.step()
+
             train_loss += loss.item() * inputs.size(0)
+
             train_bar.set_postfix({
                 'loss': f'{loss.item():.4f}',
                 'lr': f"{self.optimizer.param_groups[0]['lr']:.2e}"
             })
+
         self.scheduler.step()
         return train_loss / len(self.train_loader.dataset)
     
-    def train(self) -> None:
+    def train(self) -> torch.nn.Module:
         for epoch in range(1, self.config.epochs + 1):
             train_loss = self.train_epoch(epoch)
             val_loss, val_accuracy, report = self.validate(epoch)
-            self.log["train_loss"].append(train_loss)
-            self.log["val_loss"].append(val_loss)
-            self.log["val_accuracy"].append(val_accuracy)
+            self.log_dict["train_loss"].append(train_loss)
+            self.log_dict["val_loss"].append(val_loss)
+            self.log_dict["val_accuracy"].append(val_accuracy)
+
             print(f"\nEpoch {epoch} Summary:")
             print(f"Train Loss: {train_loss:.4f} | Val Loss: {val_loss:.4f}")
             print(f"Val Accuracy: {val_accuracy:.4f}")
@@ -188,14 +116,15 @@ class Trainer:
                 f"R={report['macro avg']['recall']:.4f} "
                 f"F1={report['macro avg']['f1-score']:.4f}\n"
             )
-
+            
             if val_loss < self.best_val_loss:
                 self.best_val_loss = val_loss
-                self.log["best_val_loss"] = self.best_val_loss
+                self.log_dict["best_val_loss"] = self.best_val_loss
                 self.save_checkpoint(epoch, val_loss, val_accuracy)
                 print(f"Checkpoint saved to {self.checkpoint_path} (Val Loss improved to {val_loss:.4f})")
             self.save_log()
         test_accuracy, final_report = self.test()
+
         print("\nFinal Test Results:")
         print(f"Test Accuracy: {test_accuracy:.4f}")
         print(
@@ -203,9 +132,10 @@ class Trainer:
             f"R={final_report['macro avg']['recall']:.4f} "
             f"F1={final_report['macro avg']['f1-score']:.4f}"
         )
-        self.log["test_accuracy"] = test_accuracy
-        self.log["test_report"] = final_report
+        self.log_dict["test_accuracy"] = test_accuracy
+        self.log_dict["test_report"] = final_report
         self.save_log()
+        return self.model
 
     def validate(self, epoch: int) -> Tuple[float, float, Dict[str, Any]]:
         self.model.eval()
@@ -224,9 +154,11 @@ class Trainer:
                 loss = self.criterion(outputs, labels)
                 val_loss += loss.item() * inputs.size(0)
                 _, preds = torch.max(outputs, 1)
+
                 all_preds.extend(preds.cpu().numpy())
                 all_labels.extend(labels.cpu().numpy())
                 val_bar.set_postfix({'val_loss': f'{loss.item():.4f}'})
+
         val_loss = val_loss / len(self.val_loader.dataset)
         report = classification_report(
             all_labels, all_preds,
@@ -254,6 +186,7 @@ class Trainer:
                 _, preds = torch.max(outputs, 1)
                 test_preds.extend(preds.cpu().numpy())
                 test_labels.extend(labels.cpu().numpy())
+
         final_report = classification_report(
             test_labels, test_preds,
             target_names=self.config.class_labels,
@@ -264,22 +197,29 @@ class Trainer:
         return test_accuracy, final_report
 
     def to_config(self) -> TrainingConfig:
-        """Возвращает текущий конфиг (можно сохранить в JSON)."""
-        return TrainingConfig(
-            batch_size=self.batch_size,
-            epochs=self.epochs,
-            lr=self.lr,
-            img_size=self.img_size,
-            random_seed=self.random_seed,
-            class_labels=self.class_labels,
-        )
+        """
+        Возвращает текущий конфиг (можно сохранить в JSON).
+        """
     
     def from_config(self, config: TrainingConfig) -> None:
-        self.batch_size = config.batch_size
-        self.epochs = config.epochs
-        self.lr = config.lr
-        self.img_size = config.img_size
-        self.random_seed = config.random_seed
-        self.class_labels = config.class_labels
-        self.split_ratio = config.split_ratio
-        self.device = config.device
+        pass
+
+    def save_checkpoint(self, epoch: int, val_loss: float, val_accuracy: float) -> None:
+        save_obj = {
+            'epoch': epoch,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'loss': val_loss,
+            'accuracy': val_accuracy
+        }
+        torch.save(save_obj, self.checkpoint_path)
+    
+    def save_log(self) -> None:
+        with open(self.log_path, "w") as f:
+            json.dump(self.log_dict, f, indent=4)
+
+    def _load_log(self) -> None:
+        if self.log_path.exists():
+            with open(self.log_path, "r") as f:
+                self.log_dict = json.load(f)
+            self.best_val_loss = self.log_dict.get("best_val_loss", float("inf"))
